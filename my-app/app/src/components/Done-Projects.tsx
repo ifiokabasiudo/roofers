6a7dd0dda5aa3b0777ed3d49722b183e5a655ae0
ifiokabasi Udo <ifiokabasiudoe@gmail.com>
2024-02-18 02:40:21 +0100

    90% done

--- my-app/app/src/components/Done-Projects.tsx
+++ my-app/app/src/components/Done-Projects.tsx
@@ -10,55 +10,46 @@ import Image8 from "../../../public/image8.jpg";
 
 export default function DoneProjects() {
   const images1: any[] = [
-    // {item: Image1},
-    // {item: Image2},
-    // {item: Image3},
-    // {item: Image4},
-    { item: "bg-1" },
-    { item: "bg-2" },
-    { item: "bg-3" },
-    { item: "bg-4" },
-    { item: "bg-5" },
-    { item: "bg-6" },
-    { item: "bg-7" },
-    { item: "bg-8" },
+    { item: Image1 },
+    { item: Image2 },
+    { item: Image3 },
+    { item: Image4 },
   ];
   const images2: any[] = [
-    { item: "bg-5" },
-    { item: "bg-6" },
-    { item: "bg-7" },
-    { item: "bg-8" },
+    { item: Image5 },
+    { item: Image6 },
+    { item: Image7 },
+    { item: Image8 },
   ];
 
   return (
     <div className="w-full min-h-[90vh] flex flex-col text-center text-[#535353] py-16 transition-all bg-white">
       <div className="font-semibold text-[#7DBF29]">PROJECTS</div>
       <h1 className="text-3xl mb-16 text-black">Done Projects</h1>
-      <div className="flex w-full overflow-hidden flex-wrap">
+      <div className="flex w-full overflow-hidden">
         {images1.map((image, index) => (
-          <div
-            key={index}
-            className={`w-full sm:w-1/2 md:w-1/4 h-52 
-            ${`bg-` + image.item}
-            bg-cover bg-no-repeat bg-center`}
-          ></div>
+          <div key={index} className="w-1/4 h-52 flex justify-center items-center">
+            <Image
+              src={image.item}
+              alt={`image-` + index.toString}
+              objectFit="cover"
+              className="w-full h-full"
+            />
+          </div>
         ))}
       </div>
-      {/* <div className="flex w-full overflow-hidden">
-            {
-                images2.map((image, index) => (
-                    <div key={index} className="w-1/4 h-44">
-                        <Image 
-                            src={image.item}
-                            alt={`image-` + index.toString} 
-                            // height={500}
-                            // width={1440}
-                            objectFit="cover"
-                        />
-                    </div>
-                ))
-            }
-        </div> */}
+      <div className="flex w-full">
+        {images2.map((image, index) => (
+          <div key={index} className="w-1/4 h-52  flex justify-center items-center">
+            <Image
+              src={image.item}
+              alt={`image-` + index.toString}
+              objectFit="cover"
+              className="w-full h-full"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
